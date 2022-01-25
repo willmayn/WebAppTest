969b9e1cf5430854a5413fd2c278b133fcdab647
Will Maynes <wmaynes@artica.com>
2022-01-25 10:24:27 -0800

    purge backend and reinit again

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "webapptest": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
